57af6ba29f6143ad29b0f6f0be6df0dd1ce35b3e
Artem Daniliants <artem.daniliants@lumolink.com>
2016-01-21 04:00:56 -0500

    Added placeholder for deploy script

---
+++ deploy.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Deployment script
+ * 
+ * @author Artem Daniliants
+ * @license http://www.gnu.org/licenses/gpl-3.0.en.html
+ * @copyright 2016 LumoSpark OÜ All Rights Reserved
+ */
+
+// Settings
